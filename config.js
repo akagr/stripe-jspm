6891d3dba3a02202d23ceb61819ef0371d096ee4
Akash Agrawal <akagr@outlook.com>
2016-01-15 16:24:35 +0530

    Pilot commit

---
+++ config.js
@@ -0,0 +1,15 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "traceur",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "jquery": "npm:jquery@2.2.0",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.93",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93"
+  }
+});
---
+++ index.js
@@ -0,0 +1,4 @@
+import 'jquery';
+import './lib/stripe';
+
+export default Stripe;
